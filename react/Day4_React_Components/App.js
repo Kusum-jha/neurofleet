cdc6a25532b76489b17524a6b9008b924724fb2b
Kusum-jha <175948219+Kusum-jha@users.noreply.github.com>
2026-02-09 19:34:41 +0530

    Create App component to render Greeting components

---
+++ react/Day4_React_Components/App.js
@@ -0,0 +1,12 @@
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Kusum" />
+      <Greeting name="React Learner" />
+    </div>
+  );
+}
+
+export default App;
